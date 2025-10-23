83c0335613438d7f51c5a95ccca32e8d03641a0f
slend12123 <artemsprincan945@gmail.com>
2025-10-23 20:08:12 +0200

    Tomcat connected and servlet created

--- src/main/java/servlet.java
+++ src/main/java/servlet.java
@@ -1,6 +1,20 @@
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/quest")
+public class servlet extends HttpServlet {
 
-public class servlet  {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
